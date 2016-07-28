a05867073aee482c0a8b721a7a0c0bf677a06023
francheues <francheuess@gmail.com>
2016-07-28 21:01:43 +0200

    Some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root 'home#welcome' 
+  
+
+  
+  resources :genres, only: :index do
+    member do
+      get 'movies'
+    end
+  end
+  resources :movies, only: [:index, :show]
 
 end
